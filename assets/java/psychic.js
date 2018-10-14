9688d25322f43dac9862574581e193955cc8ff1d
Paul Cwik <paul@cwik.us>
2018-10-14 00:32:44 -0500

    wrote the idea behind my design of the psychic game

---
+++ assets/java/psychic.js
@@ -0,0 +1,27 @@
+// game of pyschic
+
+
+// game involves the computer choosing a random letter of alphabet
+
+// var letters = [a,b,c,...z]
+// random floor number chooses index of letters array---well call this chosenLetter
+
+// using event.onkeyup = guessedLetter
+
+
+// user has ##?? incorrect guesses to figure out the letter.-- var 
+
+// if user guesses correct, add a win to round score [w, l]
+
+// if guessedLetter === chosenLetter add a win (+1 to gameScore[w])
+// else add +1 to wrongGuesses.
+
+
+// if wrongGuesses == max wrong guesses then add a loss to round score.
+
+
+// after score is updated, reset to a new round.
+// continue in loop until user is bored of game.
+
+
+// for funsies consider if guess is correct on first attempt of a given round then alert "You rock!"
