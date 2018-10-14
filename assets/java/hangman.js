0340feaa9f8e2c878b6b43edbae74ac0a5cfb347
Paul Cwik <paul@cwik.us>
2018-10-14 00:18:04 -0500

    wrote out the basic idea behind how hangman will work

---
+++ assets/java/hangman.js
@@ -0,0 +1,28 @@
+// how to hangman
+// we are going to have 3 set topics which you can choose at start of round
+// each topic is an array
+
+// topics
+// movies [top 250 off imdb]
+// bands [top 500 billboard artists]
+// world capitals [idk how many there are]
+
+
+// var userChoice = event.click.topicsChoice???
+// from userChoice, java chooses random floor number based from 0-topicChoice.length
+
+// use this number to find the item in chosen topics array.
+
+// define this as var compWord = chosenTopic[randomnumber]
+
+// create a loop to display a '_' for each letter of word unless there is a space in the word's string.
+
+// we need a few more variables
+
+// gamescore[w,l]
+
+// if var wrongGuess = 6 then user loses round.  Add a loss to gamescore.
+
+// if all letters are guessed and wrongGuess >= 5 then user wins round.  Add a win to gamescore.
+// also need to display each correct letter, replacing the '_' from earlier.
+
