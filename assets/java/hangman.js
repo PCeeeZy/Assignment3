e8d688fa7ad81d3c20fec491db69612f0da73c5c
Paul Cwik <paul@cwik.us>
2018-10-16 01:06:43 -0500

    Wrote Java for 1 functioning round of psychic

--- assets/java/hangman.js
+++ assets/java/hangman.js
@@ -1,28 +1,32 @@
 // how to hangman
-// we are going to have 3 set topics which you can choose at start of round
-// each topic is an array
+$(document).ready(function() {
 
-// topics
-// movies [top 250 off imdb]
-// bands [top 500 billboard artists]
-// world capitals [idk how many there are]
+})
+    // we are going to have 3 set topics which you can choose at start of round
+    // each topic is an array
+
+    // topics
+    // movies [top 250 off imdb]
+    // bands [top 500 billboard artists]
+    // world capitals [idk how many there are]
 
 
-// var userChoice = event.click.topicsChoice???
-// from userChoice, java chooses random floor number based from 0-topicChoice.length
+    // var userChoice = event.click.topicsChoice???
+    // from userChoice, java chooses random floor number based from 0-topicChoice.length
 
-// use this number to find the item in chosen topics array.
+    // use this number to find the item in chosen topics array.
 
-// define this as var compWord = chosenTopic[randomnumber]
+    // define this as var compWord = chosenTopic[randomnumber]
 
-// create a loop to display a '_' for each letter of word unless there is a space in the word's string.
+    // create a loop to display a '_' for each letter of word unless there is a space in the word's string.
 
-// we need a few more variables
+    // we need a few more variables
 
-// gamescore[w,l]
+    // gamescore[w,l]
 
-// if var wrongGuess = 6 then user loses round.  Add a loss to gamescore.
+    // if var wrongGuess = 6 then user loses round.  Add a loss to gamescore.
 
-// if all letters are guessed and wrongGuess >= 5 then user wins round.  Add a win to gamescore.
-// also need to display each correct letter, replacing the '_' from earlier.
+    // if all letters are guessed and wrongGuess >= 5 then user wins round.  Add a win to gamescore.
+    // also need to display each correct letter, replacing the '_' from earlier.
 
+    // HOW DO I SPLIT A STRING!!!!!!!
--- assets/java/psychic.js
+++ assets/java/psychic.js
@@ -1,27 +1,45 @@
 // game of pyschic
+$(document).ready(function() {
+
+    // game involves the computer choosing a random letter of alphabet
+
+    // var letters = [a,b,c,...z]
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var randomNumber = Math.floor(Math.random() * letters.length) + 1;
+    console.log(randomNumber);
+
+    // random floor number chooses index of letters array---well call this chosenLetter
+
+    var compLetter = letters[randomNumber];
+    console.log(compLetter);
+    
+    var wrongGuess = 0;
+    var wins = 0;
+    var losses = 0;
+
+    // using event.onkeyup = guessedLetter
+
+    document.onkeyup = function(keyInput) {
+
+        // Determines which key was pressed.
+        var userGuess = keyInput.key;
+
+        if (userGuess !== compLetter) {
+            wrongGuess ++;
+            alert("Wrong!");
+            if (wrongGuess >= "9") {
+                alert("You are not psychic!");
+                losses ++;
+                $("#lossRecord").html(losses);
+                
+            }
+        }
+        else {
+            alert("Way to go! You guessed " + userGuess + " and so did the computer!");
+            wins ++;
+            $("#winRecord").html(wins);
+        }
+    }
+})
 
 
-// game involves the computer choosing a random letter of alphabet
-
-// var letters = [a,b,c,...z]
-// random floor number chooses index of letters array---well call this chosenLetter
-
-// using event.onkeyup = guessedLetter
-
-
-// user has ##?? incorrect guesses to figure out the letter.-- var 
-
-// if user guesses correct, add a win to round score [w, l]
-
-// if guessedLetter === chosenLetter add a win (+1 to gameScore[w])
-// else add +1 to wrongGuesses.
-
-
-// if wrongGuesses == max wrong guesses then add a loss to round score.
-
-
-// after score is updated, reset to a new round.
-// continue in loop until user is bored of game.
-
-
-// for funsies consider if guess is correct on first attempt of a given round then alert "You rock!"
